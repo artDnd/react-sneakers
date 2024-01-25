009ecde4a83487a4dc37a260e7c2b4f46c8ae02c
unknown <t3ma2002@mail.ru>
2024-01-25 06:20:11 +0300

    drawer and before Axios

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div>
       <div className="card">
         <div className="favorite">
           <img src="/img/heart-unliked.svg" alt="Unliked"/>
         </div>
-        <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers"/>
-        <h5>Мужские Кроссовки Nike Blazer Mid </h5>
+        <img width={133} height={112} src={props.url} alt="Sneakers"/>
+        <h5>{props.title}</h5>
         <div className="d-flex justify-between align-center">
           <div className='d-flex flex-column'>
             <span>Цена</span>
-            <b>12 999 руб.</b>
+            <b>{props.price} руб.</b>
           </div>
           <button className="button">
             <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
